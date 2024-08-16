96c1ee7fb27096f0739046c993ced149c6d05057
arasruislam <arasru01@gmail.com>
2024-08-16 10:29:10 +0600

    hero part done

--- byte_blaze/src/components/Hero.jsx
+++ byte_blaze/src/components/Hero.jsx
@@ -1,16 +1,41 @@
+import { Link } from "react-router-dom";
+
 const Hero = () => {
    return (
       <div className="hero -mt-16">
          <div className="hero-content text-center">
             <div className="max-w-lg">
-               <h1 className="text-5xl font-bold">Welcome to ByteBlaze</h1>
+               <h1 className="text-5xl font-bold">
+                  Welcome to{" "}
+                  <span className="bg-gradient-to-r bg-300% from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text animate-gradient">
+                     ByteBlaze
+                  </span>
+               </h1>
                <p className="py-6">
                   ByteBlaze is the bridge between the complex world of
                   technology and the curious minds eager to understand it
                </p>
                <div className="flex justify-center gap-4">
-                  <button className="btn btn-primary">Read Blogs</button>
-                  <button className="btn btn-primary">Bookmarks</button>
+                  <Link
+                     to="/blogs"
+                     class="relative inline-block px-4 py-2 font-medium group"
+                  >
+                     <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-primary group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                     <span class="absolute inset-0 w-full h-full bg-white border-2 border-secondary group-hover:bg-primary"></span>
+                     <span class="relative text-black group-hover:text-white">
+                        Read Blogs
+                     </span>
+                  </Link>
+                  <Link
+                     to="/bookmarks"
+                     class="relative inline-block px-4 py-2 font-medium group"
+                  >
+                     <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-primary group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                     <span class="absolute inset-0 w-full h-full bg-white border-2 border-secondary group-hover:bg-primary"></span>
+                     <span class="relative text-black group-hover:text-white">
+                        Bookmarks
+                     </span>
+                  </Link>
                </div>
             </div>
          </div>
