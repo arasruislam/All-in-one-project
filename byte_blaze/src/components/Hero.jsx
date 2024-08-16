dbd20d168b77e32a5836c23c8537dde74ecef7e9
arasruislam <arasru01@gmail.com>
2024-08-16 10:21:54 +0600

    update home

--- byte_blaze/src/components/Hero.jsx
+++ byte_blaze/src/components/Hero.jsx
@@ -1,8 +1,8 @@
 const Hero = () => {
    return (
-      <div className="hero bg-base-200">
+      <div className="hero -mt-16">
          <div className="hero-content text-center">
-            <div className="max-w-md">
+            <div className="max-w-lg">
                <h1 className="text-5xl font-bold">Welcome to ByteBlaze</h1>
                <p className="py-6">
                   ByteBlaze is the bridge between the complex world of
--- byte_blaze/src/pages/Home.jsx
+++ byte_blaze/src/pages/Home.jsx
@@ -4,9 +4,9 @@ import Hero from "../components/Hero";
 
 const Home = () => {
    return (
-      <>
+      <div className="min-h-[calc(100vh-116px)] flex justify-center items-center">
          <Hero />
-      </>
+      </div>
    );
 };
 
