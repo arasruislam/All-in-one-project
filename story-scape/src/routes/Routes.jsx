003002e91571a057642024fc6c754bdb9abbf9ce
arasruislam <arasru01@gmail.com>
2024-08-19 00:31:42 +0600

    create another page pageToRead

--- story-scape/src/routes/Routes.jsx
+++ story-scape/src/routes/Routes.jsx
@@ -5,6 +5,7 @@ import ErrorPage from "../pages/ErrorPage";
 import BookDetails from "../components/Cards/BookDetails";
 import ListedBooks from "../pages/ListedBooks";
 import ListBook from "../components/Cards/ListBook";
+import PageToRead from "../pages/PageToRead";
 
 const router = createBrowserRouter([
    {
@@ -24,6 +25,10 @@ const router = createBrowserRouter([
          {
             path: "/listed_book",
             element: <ListedBooks />,
+         },
+         {
+            path: "/page_to_read",
+            element: <PageToRead />,
          },
       ],
    },
