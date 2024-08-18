123d7cbebe85437028ce3730b43f150ce0f1953c
arasruislam <arasru01@gmail.com>
2024-08-18 11:00:02 +0600

    footer done

--- story-scape/src/components/Shared/Footer.jsx
+++ story-scape/src/components/Shared/Footer.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
    return (
-      <div>
-         
-      </div>
+      <footer className="footer bg-base-300 text-base-content items-center p-4 rounded">
+         <aside className="grid-flow-col items-center">
+            <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+         </aside>
+         <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+            <a>
+               <FaFacebook size={25} />
+            </a>
+            <a>
+               <FaGithub size={25} />
+            </a>
+            <a>
+               <FaLinkedin size={25} />
+            </a>
+         </nav>
+      </footer>
    );
 };
 
--- story-scape/src/layout/Primary.jsx
+++ story-scape/src/layout/Primary.jsx
@@ -1,8 +1,19 @@
 import React from "react";
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
+import Header from "../components/Shared/Header";
+import Footer from "../components/Shared/Footer";
 
 const Primary = () => {
-   return <div><Outlet/></div>;
+   
+   return (
+      <div className="max-w-[2560px] mx-auto xl:px-16 md:px-8 sm:px-2 px-0">
+         <Header />
+         <div>
+            <Outlet />
+         </div>
+         <Footer />
+      </div>
+   );
 };
 
 export default Primary;
